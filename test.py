dc1d514ab12f26f1379e90562dd93bea0c9500e4
kimhyeji <hyjias@naver.com>
2021-06-04 11:26:06 +0900

    even

---
+++ test.py
@@ -0,0 +1,12 @@
+ function even() {
+	let arr = [];
+	for (let i = 0; i <= 50; i++) {
+	    if ((i%2) == 0 ) {
+		arr.push(i) }
+	    else if ((i%2) !== 0) {
+            continue
+	     } 
+        }  
+ return arr;
+}
+ even()
